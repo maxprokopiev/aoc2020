75ca7627e7efcb1cf06dff00d6f9e6188d79c2f9
Max Prokopyev <me@maxprokopiev.com>
2021-01-05 00:03:23 +0100

    Day 24 part 2

--- lib/day24.ex
+++ lib/day24.ex
@@ -11,12 +11,51 @@ defmodule Day24 do
   def solve(input) do
     paths = Enum.map input, fn s -> prepare_paths(s) end
 
-    new_floor = Enum.reduce paths, Map.put(%{}, {0, 0, 0}, false), fn path, floor ->
+    f1 = Enum.reduce paths, Map.put(%{}, {0, 0, 0}, false), fn path, floor ->
       coords = traverse({0, 0, 0}, path)
       Map.put(floor, coords, !floor[coords])
     end
 
-    Enum.count Map.values(new_floor), fn t -> t end
+    Enum.count Map.values(f1), fn t -> t end
+  end
+
+  def solve2(input) do
+    paths = Enum.map input, fn s -> prepare_paths(s) end
+
+    f1 = Enum.reduce paths, Map.put(%{}, {0, 0, 0}, false), fn path, floor ->
+      coords = traverse({0, 0, 0}, path)
+      Map.put(floor, coords, !floor[coords])
+    end
+
+    f2 = Enum.reduce 1..100, f1, fn _, nf ->
+      iterate(nf)
+    end
+    Enum.count Map.values(f2), fn t -> t end
+  end
+
+  def iterate(floor) do
+    extended_floor = Enum.reduce Map.keys(floor), floor, fn {x, y, z}, f ->
+      ns = Enum.map Map.values(@coords), fn {dx, dy, dz} -> {x + dx, y + dy, z + dz} end
+      Enum.reduce ns, f, fn nb, nf ->
+        if nf[nb] == nil, do: Map.put(nf, nb, false), else: nf
+      end
+    end
+
+    r = Enum.map extended_floor, fn {coords, black} ->
+      n = count_black_neighbours(extended_floor, coords)
+      v = case black do
+        true -> !((n == 0) || (n > 2))
+        false -> n == 2
+      end
+
+      {coords, v}
+    end
+    Enum.into(r, %{})
+  end
+
+  def count_black_neighbours(floor, coords = {x, y, z}) do
+    ns = Enum.map Map.values(@coords), fn {dx, dy, dz} -> {x + dx, y + dy, z + dz} end
+    Enum.count ns, fn c -> floor[c] == true end
   end
 
   def prepare_paths(s, i \\ 0, paths \\ []) do
--- test/day24_test.exs
+++ test/day24_test.exs
@@ -8,10 +8,24 @@ defmodule Day24Test do
     assert Day24.solve(input) == 10
   end
 
+  test "sample part2" do
+    {:ok, content} = File.read("test/resources/day24_sample.txt")
+    input = content |> String.split("\n", trim: true)
+
+    assert Day24.solve2(input) == 2208
+  end
+
   test "puzzle" do
     {:ok, content} = File.read("test/resources/day24.txt")
     input = content |> String.split("\n", trim: true)
 
     assert Day24.solve(input) == 300
+  end
+
+  test "puzzle part2" do
+    {:ok, content} = File.read("test/resources/day24.txt")
+    input = content |> String.split("\n", trim: true)
+
+    assert Day24.solve2(input) == 3466
   end
 end
